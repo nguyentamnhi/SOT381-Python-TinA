9cb16143148fb001d908a97f741f37cc4bfce034
nguyentamnhi <nhi.nt.67khmt@ntu.edu.vn>
2025-12-22 14:34:49 +0700

    Update TH2212_Bai1.py

--- TH2212_Bai1.py
+++ TH2212_Bai1.py
@@ -2,10 +2,17 @@ while True:
  w = float(input(" Nhập chiều rộng w= "))
  if 0.0<=w<=100.0:
    break
+ if (w<=0.0) and (w<=100.0) :
+     break
+ else:   
+  print(" sai, nhập lại: ")
 while True:
  h = float(input(" Nhập chiều dài h= "))
  if 0.0<=h<=100.0:
    break
+ else:
+     print(" sai, nhập lại:")
+ 
 chu_vi = ( w + h )*2
 dien_tich = w*h
 print(f" chu vi hình chữ nhật:{chu_vi:.2f}")
